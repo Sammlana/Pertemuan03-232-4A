b2bd9e396ff55c8f80976430a355f57323cffc61
Sammlana <jokolkkpk@gmail.com>
2024-04-22 12:18:21 +0800

    3

--- InputOutput.java
+++ InputOutput.java
@@ -4,7 +4,7 @@ public class InputOutput{
         public static void main(String[] args){
             int nilai = 0;
             double rata = 0;
-            String nama = 'Hisyam'
+            String nama = 'Hisyam';
             Scanner scanner
         }
 }
