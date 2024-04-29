bc8aed18a697b21b1ef0d49e9ae73a7a9d8ee323
Sammlana <jokolkkpk@gmail.com>
2024-04-29 17:13:22 +0800

    29-04 part 2

--- OperasiString.java
+++ OperasiString.java
@@ -25,6 +25,47 @@ public class OperasiString{
 
         System.out.println(tanggal+ "-" +bulan+ "-" +tahun);
 
-
+        switch(bulan){
+            case "01":
+                System.out.println(tanggal+ " Januari "+tahun);
+                break;
+            case "02":
+                System.out.println(tanggal+ " Februari "+tahun);
+                break;
+            case "03":
+                System.out.println(tanggal+ " Maret "+tahun);
+                break;
+            case "04":
+                System.out.println(tanggal+ " April "+tahun);
+                break;
+            case "05":
+                System.out.println(tanggal+ " Mei "+tahun);
+                break;
+            case "06":
+                System.out.println(tanggal+ " Juni "+tahun);
+                break;
+            case "07":
+                System.out.println(tanggal+ " Juli "+tahun);
+                break;
+            case "08":
+                System.out.println(tanggal+ " Agustus "+tahun);
+                break;
+            case "09":
+                System.out.println(tanggal+ " September "+tahun);
+                break;
+            case "10":
+                System.out.println(tanggal+ " Oktober "+tahun);
+                break;
+            case "11":
+                System.out.println(tanggal+ " November "+tahun);
+                break;
+            case "12":
+                System.out.println(tanggal+ " Desember "+tahun);
+                break;
+            default:
+                System.out.println("Tidak Valid");
+                break;                
+        }
+    
     }
 }
